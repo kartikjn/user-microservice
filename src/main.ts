b6ec83fe92df1849f77367243f3d1496c1738f5e
Nikhil Meshram <nikhilmeshram@rapidinnovation.dev>
2023-01-13 17:48:20 +0530

    refactor:chnages in module and main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    package: 'userdata',
+    protoPath: join(__dirname, '../src/user.proto'),
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app =  await NestFactory.createMicroservice(AppModule, microserviceOptions);
+  await app.listen();
 }
 bootstrap();

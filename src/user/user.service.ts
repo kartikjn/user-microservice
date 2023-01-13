3dc9cfc4ca1bdf9327198badf9ed61fb4fe4ae2c
kartikjn <kj@rapidinnovation.dev>
2023-01-13 16:42:58 +0530

    feat: userRegistration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  
+  constructor(
+    @InjectRepository(UserEntity) 
+    private repository: Repository<UserEntity>
+  ) {}
+
+  async signup(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const { email, username, password } = createUserDto;
+    const user = new UserEntity();
+
+    user.email = email;
+    user.username = username;
+    user.password = password
+    await this.repository.save(user);
+    
+    return user;
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }

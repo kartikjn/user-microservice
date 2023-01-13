9252f40acc44aec88c55738a6c2125bc98bc2a3a
kartikjn <kj@rapidinnovation.dev>
2023-01-13 16:05:06 +0530

    started microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { config } from 'dotenv';
+
+config({ path: join(__dirname, '../.env') });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+      transport: Transport.GRPC,
+      options: {
+          url: `localhost:${process.env.GRPC_PORT}`,
+          package: 'users',
+          protoPath: join(__dirname, '../src/_proto/user.proto'),
+          maxReceiveMessageLength:
+              Number(process.env.GRPC_MAX_MESSAGE_SIZE_BYTES) || 21000000,
+          maxSendMessageLength:
+              Number(process.env.GRPC_MAX_MESSAGE_SIZE_BYTES) || 21000000
+      }
+  });
+
+  await app.listen();
 }
+
 bootstrap();

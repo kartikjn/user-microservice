c8e97d22c9dce81b2e65f6b793da09903a99c59f
kartikjn <kj@rapidinnovation.dev>
2023-01-13 16:28:16 +0530

    refactored

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserService } from './app.service';
 import { typeOrmConfig } from './config/typeOrm.config';
-import { User } from './entity/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 

83183642c37ac6e73f4ebc9d48d66c594e591d9d
kartikjn <kj@rapidinnovation.dev>
2023-01-13 16:54:00 +0530

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UserService } from './app.service';
+import { UserService } from './user/user.service';
 import { typeOrmConfig } from './config/typeOrm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';

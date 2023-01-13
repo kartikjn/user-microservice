bf7d351aee224f2cf9b74174bf4f4052fa93cf54
kartikjn <kj@rapidinnovation.dev>
2023-01-13 16:28:36 +0530

    userdto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    userID: string;
+
+    @IsNotEmpty()
+    @MinLength(8)
+    password: string;
+}
